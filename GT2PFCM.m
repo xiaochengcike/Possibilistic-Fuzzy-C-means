ab87a4def41745897f8a004c7330d06d1efa41fb
Atharva Amdekar <amdekar.atharva@gmail.com>
2018-07-10 21:17:51 +0900

    Added functions for calculating classification rate and alpha cuts

--- GT2PFCM.m
+++ GT2PFCM.m
@@ -1,51 +1,104 @@
-c = 3;              %number of clusters
-n = 100;            %number of points
-d = 2;              %number of dimensions
+% c = 3;              %number of clusters
+% n = 100;            %number of points
+% d = 2;              %number of dimensions
 % X = rand(n,d);     
 a = 2;              %user defined const a in objective function
 b = 5;             %user defined const b in objective function
-m = 2;              %fuzzifier
-eta = 2;            %pcm uncertainty parameter for type 1
+m = 2;              %fuzzifier only for type 1  
+eta = 7;            %pcm uncertainty parameter for type 1
 epsilon = 10^(-4); %error threshold
 max_iter = 1000;   %max number of iterations before exit
 eta1 = 2;
 eta2 = 3;          %UNCERTAINTY PARAMETERS FOR IT2 PFCM [m1,m2] & [eta1, eta2]
 m1 = 2;
-m2 = 3;
-run_pfcm_type1 = 1;     %Boolean for PFCM Type-1
-run_pfcm_intervaltype2 = 1;     %Boolean for PFCM Type-2
-labelled_dataset = 0;     %For datasets such as Iris, Breast Cancer etc
+m2 = 7;
+run_pfcm_type1 = 0;     %Boolean for PFCM Type-1
+run_pfcm_intervaltype2 = 0;     %Boolean for PFCM Type-2
+run_pfcm_generaltype2 = 1;   %Boolean for GT2 PFCM
+labelled_dataset = 1;     %For datasets such as Iris, Breast Cancer etc
 unlabelled_dataset = 0;   %For datasets such as Squares3Clust
-random_X = 1;             %For randomly generated X
+random_X = 0;             %For randomly generated X
 normalrandom_X = 0;       %For normal random X
+graph_2d_bool = 0;
+graph_2d_conv = 0;
+graph_3d_conv = 0;
+graph_3d_bool = 1;
+purity_checking_bool=1;%gives the purity checking error rate
+f1_score_bool=1;%gives the f1 score
+classification_rate_array=[purity_checking_bool f1_score_bool];%this stores which error rates to print
+mean_m = 4;
+std_dev_m = 1;          %mean and std deviation for fuzzifier m
+mean_eta = 2;
+std_dev_eta = 0.3;      %mean and std deviation for eta
+alpha = 0.1:0.2:0.9;    %alpha values
+m_array = 1:1:15        %range of fuzzifier values
+eta_array = 1:1:15;    %range of eta values
 
 
 if random_X
-    X = rand(n,d);
+    c = 3;              %number of clusters
+    n = 100;            %number of points
+    d = 2;              %number of dimensions
+     X = rand(n,d);
 end
 
 if normalrandom_X
+    c = 3;              %number of clusters
+    n = 100;            %number of points
+    d = 2;              %number of dimensions
     X = randn(n,d);
 end
 
 if labelled_dataset
-    [X,Y]=get_data_iris('iris-dataset.txt',',',5);
+%     [X,y]=get_data_iris('iris-dataset.txt',',',5);
+        [X y]=get_text_data('breastCancer.txt',9);%getting data from breast cancer dataset
+%     [X Y]=get_text_data('Bridge.txt',3);%getting data for bridge data dataset
+    c = size(unique(y),1);
+    d = size(X,2);
+    n = size(X,1);
+    
+end
+
+if run_pfcm_generaltype2
+    [V_init U] = get_final_values_fcm(X,c,n,d,m,epsilon,max_iter);
+    gamma = calculate_gamma(X,V_init,U,c,n,m);
+    [V_gt2 U_gt2 T] = get_final_values_pfcm_generaltype2(X,c,n,d,alpha,m_array, mean_m,std_dev_m,eta_array,mean_eta,std_dev_eta,a,b,gamma,epsilon,max_iter,V_init);
     
 end
 
 if run_pfcm_intervaltype2
     [V U] = get_final_values_fcm(X,c,n,d,m,epsilon,max_iter);
     gamma = calculate_gamma(X,V,U,c,n,m);
-    [V U T] = get_final_values_pfcm_intervaltype2(X,c,n,d,m1,m2,eta1,eta2,a,b,gamma,epsilon,max_iter,V);
-    graph_2d(X,V,U);
+    [V_it2 U_it2 T] = get_final_values_pfcm_intervaltype2(X,c,n,d,m1,m2,eta1,eta2,a,b,gamma,epsilon,max_iter,V);
+    
+    if graph_2d_bool
+       graph_2d(X,V_it2,U_it2);
+    end
+    if graph_3d_bool 
+        graph_3d(X,V_it2,U_it2,'g',200,'o',1);
+    end
+    if labelled_dataset
+        classification_rate(X,V_it2,U_it2,y,classification_rate_array);
+    end
 %     graph_2d(X,V,U,'r');
 end
+
 if run_pfcm_type1
     [V U] = get_final_values_fcm(X,c,n,d,m,epsilon,max_iter);
     gamma = calculate_gamma(X,V,U,c,n,m);
-    [V U T] = get_final_values_pfcm_type1(X,c,n,d,m,eta,a,b,gamma,epsilon,max_iter,V);
-    figure;
-    graph_2d(X,V,U);
+    [V_t1 U_t1 T] = get_final_values_pfcm_type1(X,c,n,d,m,eta,a,b,gamma,epsilon,max_iter,V);
+   
+    if graph_2d_bool
+         figure;
+       graph_2d(X,V_t1,U_t1);
+    end
+    if graph_3d_bool
+        figure;
+        graph_3d(X,V_t1,U_t1,'g',200,'o',1);
+    end
+    if labelled_dataset
+        classification_rate(X,V_t1,U_t1,y,classification_rate_array);
+    end
 %     graph_2d(X,V,U,'r');
 end
 
@@ -138,15 +191,16 @@ end
 
 function [V U T] = get_final_values_pfcm_type1(X,c,n,d,m,eta,a,b,gamma,epsilon,max_iter,V_init)
     V_old = V_init;
+%     V_old = rand(c,d);
     V = zeros(c,d);
     U = zeros(c,d);
     err = 10000;
     for iterations = 1:max_iter
-        iterations
+%         iterations
         U = membership_update(X,V_old,m,c,n);           %U is cxn membership matrix
-        T = typicality_update(X,V_old,eta,b,c,n,gamma)    %T is cxn typicality matrix
-        V = cluster_update(X,a,b,U,T,m,eta,c,n,d)     %V is cxd matrix containing cluster centers
-        err = abs(sum(sum(V-V_old)))
+        T = typicality_update(X,V_old,eta,b,c,n,gamma);    %T is cxn typicality matrix
+        V = cluster_update(X,a,b,U,T,m,eta,c,n,d) ;    %V is cxd matrix containing cluster centers
+        err = abs(sum(sum(V-V_old)));
         if err<epsilon
             break
         end
@@ -160,7 +214,7 @@ function [V U T] = get_final_values_pfcm_intervaltype2(X,c,n,d,m1,m2,eta1,eta2,a
     U = zeros(c,d);
     err = 10000;
     for iterations = 1:max_iter
-        iterations
+%         iterations
         U_m1 = membership_update(X,V_old,m1,c,n);           %U is cxn membership matrix
         U_m2 = membership_update(X,V_old,m2,c,n);
         U = (U_m1+U_m2)/2.0;
@@ -169,12 +223,28 @@ function [V U T] = get_final_values_pfcm_intervaltype2(X,c,n,d,m1,m2,eta1,eta2,a
         T_2 = typicality_update(X,V_old,eta2,b,c,n,gamma);     
         [T_L T_R] = get_leftright_fuzzified(T_1,T_2,eta1,eta2); %T_L and T_R are fuzzified and ordered typicality values to be passed to eiasc
         T = (T_1+T_2)/2.0;
-        V = eiascc(X, U_L, U_R, T_L, T_R, n, c, d, a, b)     %V is cxd matrix containing cluster centers
-        err = abs(sum(sum(V-V_old)))
+        V = eiascc(X, U_L, U_R, T_L, T_R, n, c, d, a, b) ;    %V is cxd matrix containing cluster centers
+        err = abs(sum(sum(V-V_old)));
         if err<epsilon
             break
         end
         V_old = V;
+    end
+end
+
+function [V U T] = get_final_values_pfcm_generaltype2(X,c,n,d,alpha,m_array, mean_m,std_dev_m,eta_array,mean_eta,std_dev_eta,a,b,gamma,epsilon,max_iter,V_init) 
+    U_fuzz = gaussian(m_array,std_dev_m,mean_m,0);
+    U_eta =  gaussian(m_array,std_dev_m,mean_m,0);
+    m_list = alpha_cut(m_array,U_fuzz,std_dev_m,mean_m,alpha,0)
+    eta_list = alpha_cut(eta_array, U_eta, std_dev_eta, mean_eta, alpha,0)
+    
+end
+
+function Y=gaussian(X,variance,mean,graph_bool)
+    Y=gaussmf(X,[variance mean]);%gaussian graph
+    if graph_bool
+        figure(1);
+        plot(X,Y);
     end
 end
 
@@ -270,6 +340,132 @@ function y_all = eiascc(X, U_L, U_R, T_L, T_R, n, c, d, acons, bcons) %y_all is 
             y_all(clusters,dim) = (y_l+y_r)/2;
         end
     end
+end
+
+function [tp fp fn tn]= binary_comparison(calculated_labels,y)
+
+        t_labels=calculated_labels==max(calculated_labels);
+        t_y=(y==max(y))*2;
+        tp=sum((t_labels-t_y)==-1);
+        fp=sum((t_labels-t_y)==1);
+        fn=sum((t_labels-t_y)==-2);
+        tn=sum((t_labels-t_y)==0);
+end
+
+function [X Y]=get_text_data(filename,y_column)
+    data=importdata(filename);
+    Y=data(:,y_column);
+    X=data(:,1:(y_column-1));
+end
+
+function [error precision recall f1_score] = classification_rate(X,V,U,y,classification_rate_array)
+    y_temp=[];
+    y_count=0;
+    binary_classification=0;
+    for unique_y=[unique(y)]'
+        y_count=y_count+1;
+        y_temp=[y_temp ;repmat(y_count,sum(unique_y==y),1)];
+    end
+    y=y_temp;
+    y_list=[];
+    calculated_labels=max((max(U)==U).*linspace(1,size(U,1),size(U,1))')';
+    index=0;
+    accuracy=0;
+    labels_position=zeros(size(unique(y)));
+    unique_y=unique(y);
+    for unique_y_index=[unique(y)]'
+        max_classifications=0;
+        for unique_y_inner_index=[unique(y)]'
+            correct_classifications=sum(calculated_labels(index+1:index+sum(unique_y(unique_y_index)==y))==unique_y(unique_y_inner_index) );
+            if max_classifications<correct_classifications
+                max_classifications=correct_classifications;
+                labels_position(unique_y_index)=unique_y_inner_index;
+            end    
+        end 
+        index=index+sum(y==unique_y(unique_y_index));%this needs to be updated
+    end
+
+    y_temp=zeros(size(y));
+    for labels_index=labels_position'
+        y_temp(find(calculated_labels==labels_position(labels_index)))=labels_index;
+    end   
+%     unique(y)';
+%     histc(y,unique(y))';
+%     unique(calculated_labels)';
+%     histc(calculated_labels,unique(calculated_labels))';
+%     [a b]=sort(histc(y,unique(y))');
+%     [c d]=sort(histc(calculated_labels,unique(calculated_labels))');
+%     y_temp=zeros(size(y));
+%     for i=1:size(a,2)
+%         y_temp=y_temp+(calculated_labels==d(i)).*b(i);
+%     end
+%     a=(sort(histc(calculated_labels,unique(calculated_labels))')-sort(histc(y,unique(y))'))
+%     error=(sum((a>0).*a)/size(y,1))*100        
+  
+    calculated_labels=y_temp;
+   
+    if classification_rate_array(1)%first value of classification_rate array telsl about the purity error rate
+        error=(1-sum(calculated_labels==y)/size(y,1))*100;
+        disp("purity check error rate="+error);
+    end
+
+    if classification_rate_array(2)
+        unique_y=unique(y);
+        confusion_matrix=zeros(size(unique_y,1));
+        for unique_y_index=1:size(unique_y,1)
+            for unique_y_index_inner=1:size(unique_y,1)
+                a=(y==unique_y(unique_y_index));
+                b=(calculated_labels==unique_y(unique_y_index_inner));
+                tp=binary_comparison(b,a);
+%                 confusion_matrix(unique_y(unique_y_index_inner),unique_y(unique_y_index))=binary_comparison( (calculated_labels==unique_y(unique_y_index_inner)), (y==unique_y(unique_y_index)));
+                confusion_matrix(unique_y(unique_y_index_inner),unique_y(unique_y_index))=tp;
+                
+            end        
+        end
+        confusion_matrix
+         if size(unique(calculated_labels),1)==2
+            binary_classification=1;
+            [tp fp fn tn]=binary_comparison((~(calculated_labels==max(calculated_labels)))*max(calculated_labels)+(calculated_labels==max(calculated_labels))*min(calculated_labels),(~(y==max(y)))*max(y)+(y==max(y))*min(y));
+        end
+        if binary_classification
+            precision=tp/(tp+fp);
+            recall=tp/(tp+fn);
+            f1_score=2*(precision*recall)/(precision+recall);
+            disp("precision="+precision);
+            disp("recall="+recall);
+            disp("F1 score="+f1_score);
+        end
+    end
+end
+
+function m_list=alpha_cut(m_array,U,standard_deviation,mean,alpha,graph_bool)%returns the values of m for the alpha cuts %0 graph bool means no grpah else graph
+    syms m;
+    gauss(m)=gaussmf(m,[standard_deviation mean]);
+    gaussinv=finverse(gauss);
+    F_right=matlabFunction(gaussinv);%F_right is the right side of the function
+    F_left=matlabFunction(-(gaussinv-mean)+mean);%F_left is the left side of the function
+    X_right=F_right([alpha]);
+    X_left=F_left([alpha]);
+    if graph_bool
+        hold on
+        plot(m_array,U);
+        plot(X_right,[alpha 1-alpha],"*");
+        plot(X_left,[alpha 1-alpha],"*");
+        x_line=X_left(1):0.01:X_right(1);
+        y_line=repmat(alpha,size(x_line));
+        p1=plot(x_line,y_line);
+        x_line=X_left(2):0.01:X_right(2);
+        y_line=repmat(1-alpha,size(x_line));
+        p2=plot(x_line,y_line);
+        ylabel('Membership Values of m','FontSize' , 15,'FontWeight' , 'bold');
+        xlabel('Fuzzifier (m)','FontSize' , 15,'FontWeight' , 'bold');
+        lgd=legend([p1 p2],'alpha cut','1-alpha cut'); 
+        lgd.FontSize = 12;
+        set([p1 p2],'LineWidth',2);
+        set(gca,'fontsize',15);
+        hold off
+    end
+    m_list=[X_left fliplr(X_right)] ;%m_list is 1*(2*(number of alpha cuts)) vectors in ascending order of m
 end
 
 function [X Y]=get_data_iris(filename,delimiter,y_column)%gettomg data from a file separated with a delimiter named filename and the labels are in y_columbn
